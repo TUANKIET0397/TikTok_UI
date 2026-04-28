7c051e8ddbdf4f1b30b230347f2833104a213eb4
TUANKIET0397 <kiet8103@gmail.com>
2026-04-28 23:34:32 +0700

    repair vite.config, add jsconfig.json for auto recommend codee

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite"
+// import { path } from "node:path"
+import { fileURLToPath, URL } from "node:url"
 import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
@@ -7,5 +9,18 @@ export default defineConfig({
     server: {
         port: 3000,
         strictPort: true,
+        proxy: {
+            "/api": "http://localhost:8000", //fetch('/api/products')
+        },
+    },
+    resolve: {
+        alias: {
+            // "@": "/src",
+            // "@": path.resolve(__dirname, "./src"),
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        outDir: "build", //change khi build production
     },
 })

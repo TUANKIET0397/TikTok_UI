ae19d0f7a90944f6e530c1bd19589ae4cb5fcc33
TUANKIET0397 <kiet8103@gmail.com>
2026-04-29 23:15:00 +0700

    change position of route, put the route out

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
-import Following from '@/pages/Following';
-
+import { publicRoutes } from '@/routes';
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/following" element={<Following />} />
+                {publicRoutes.map((route, index) => {
+                    const Page = route.component;
+                    return <Route key={index} path={route.path} element={<Page />} />;
+                })}
             </Routes>
         </BrowserRouter>
     );
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,24 @@
-const publicRoutes = [];
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Profile from '@/pages/Profile';
 
+// Không cần đăng nhập vẫn có thể xem được trang chủ và trang following
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+];
+
+// Cần đăng nhập mới có thể xem được trang profile, upload video, ... nên để privateRoutes
 const privateRoutes = [];
 
 export { publicRoutes, privateRoutes };

f8118d0138dc1cd912737cf142008c3bf33dc199
TUANKIET0397 <kiet8103@gmail.com>
2026-04-30 00:06:58 +0700

    build load layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { Fragment } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '@/routes';
+import { DefaultLayout } from '@/components/Layout';
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 {publicRoutes.map((route, index) => {
+                    // const Layout = route.layout === null ? Fragment : DefaultLayout;
+
+                    const layout = DefaultLayout;
+                    if (route.layout) {
+                        layout = route.layout;
+                    } else if (route.layout === null) {
+                        layout = Fragment;
+                    }
+
                     const Page = route.component;
-                    return <Route key={index} path={route.path} element={<Page />} />;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
                 })}
             </Routes>
         </BrowserRouter>
